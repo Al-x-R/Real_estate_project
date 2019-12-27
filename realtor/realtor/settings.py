8cc8ff0eccc1358d728c2cb9f3729eb36d3254a0
Al-x-R <alexander.roshchin85@gmail.com>
2019-12-27 15:24:39 +0200

    modified

--- realtor/realtor/settings.py
+++ realtor/realtor/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'qbzdxgf96sbve!)!-y8ccb&$f-!9(a7&uz+85%n!hs*46*b+6x'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
